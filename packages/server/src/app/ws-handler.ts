f800d1af5e579ad2eee54815af0fd8f7f8890dd1
Miguel Acosta <dev.miguel.acosta@gmail.com>
2023-05-05 18:11:44 -0500

    feature: setup websocket server to receive a message and to log on events like close

---
+++ packages/server/src/app/ws-handler.ts
@@ -0,0 +1,36 @@
+import { IncomingMessage } from 'http';
+import { WebSocket, WebSocketServer, ServerOptions, RawData } from 'ws';
+
+export class WsHandler {
+  private wsServer: WebSocketServer;
+
+  initialize(options: ServerOptions) {
+    this.wsServer = new WebSocketServer(options);
+
+    this.wsServer.on('listening', () =>
+      console.log(`Server listening on port ${options.port}`)
+    );
+    this.wsServer.on('connection', (socket, request) =>
+      this.onSocketConnected(socket, request)
+    );
+  }
+
+  onSocketConnected(socket: WebSocket, request: IncomingMessage) {
+    console.log('New webSocket connection');
+
+    socket.on('message', (data) => this.onSocketMessage(socket, data));
+    socket.on('close', (code, reason) => this.onSocketClosed(code, reason));
+  }
+
+  onSocketMessage(socket: WebSocket, data: RawData) {
+    const payload = JSON.parse(`${data}`);
+    console.log('Received: ', payload);
+
+    const reply = JSON.stringify({ reply: 'Message received successfully' });
+    socket.send(reply);
+  }
+
+  onSocketClosed(code: number, reason: Buffer) {
+    console.log(`Client has disconnected; code=${code}, reason=${reason}`);
+  }
+}
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+import { ServerOptions } from 'ws';
+import { WsHandler } from './app/ws-handler';
+
+function main() {
+  const options: ServerOptions = { port: 8080 };
+  const handler = new WsHandler();
+  handler.initialize(options);
+}
+
+main();
